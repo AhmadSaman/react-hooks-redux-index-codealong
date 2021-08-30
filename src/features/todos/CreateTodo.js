981eadc328a0b02aa35813db9384f883ac375ec7
AhmadSaman <ahmad.00079821@gmail.com>
2021-08-30 12:36:48 +0300

    commit tree 16fa4110c2265c75b8d43c5eaf64e7b4165b1d5a

--- src/features/todos/CreateTodo.js
+++ src/features/todos/CreateTodo.js
@@ -3,30 +3,31 @@ import { useDispatch } from "react-redux";
 import { todoAdded } from "./todosSlice";
 
 function CreateTodo() {
-  const [text, setText] = useState("");
-  const dispatch = useDispatch();
+	const [text, setText] = useState("");
+	const dispatch = useDispatch();
 
-  function handleChange(event) {
-    setText(event.target.value);
-  }
+	function handleChange(event) {
+		setText(event.target.value);
+	}
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    dispatch(todoAdded(text));
-  }
+	function handleSubmit(event) {
+		event.preventDefault();
+		dispatch(todoAdded(text));
+		setText("");
+	}
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <p>
-          <label>add todo</label>
-          <input type="text" onChange={handleChange} value={text} />
-        </p>
-        <input type="submit" />
-      </form>
-      <p>Form Text: {text}</p>
-    </div>
-  );
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<p>
+					<label>add todo</label>
+					<input type="text" onChange={handleChange} value={text} />
+				</p>
+				<input type="submit" />
+			</form>
+			<p>Form Text: {text}</p>
+		</div>
+	);
 }
 
 export default CreateTodo;
